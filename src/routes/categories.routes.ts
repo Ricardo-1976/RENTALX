80a1e35fe2d94845b1c9143b29e7fd06c71b14b0
Ricardo-1976 <ricardojoaoantonio2@gmail.com>
2021-12-12 22:36:08 +0100

    listagem

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,31 @@
-import { Router } from "express";
+import { request, Router } from "express";
 import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
-
+// Create category
 categoriesRoutes.post("/", (request, response) => {
   const {name, description} = request.body;
 
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date()
-  });
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  categories.push(category);
+  if(categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already Exist!" })
+  }
 
-  return response.status(201).json(category);
+  categoriesRepository.create({ name,  description });
 
+  return response.status(201).send();
+
+});
+
+// List Category
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
+
+  return response.json(all);
 });
 
 export { categoriesRoutes };
