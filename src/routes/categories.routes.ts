7a1480fb31321b282fdc7a32fe7dbb7d022da3c7
Ricardo-1976 <ricardojoaoantonio2@gmail.com>
2021-12-21 00:44:23 +0100

    list category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,28 +1,21 @@
 import { request, Router } from "express";
+
 import { Category } from "../modules/cars/model/Category";
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/serveses/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 // Create category
 categoriesRoutes.post("/", (request, response) => {
-  const {name, description} = request.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return response.status(201).send();
-
+    return createCategoryController.handle(request, response);
 });
 
 // List Category
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-
-  return response.json(all);
+  return listCategoriesController.handle(request, response);
 });
 
 export { categoriesRoutes };
