c283898f56ba33ec5818ed05a07ab8dadd501002
Ricardo-1976 <ricardojoaoantonio2@gmail.com>
2021-12-27 23:07:19 +0100

    csvParse

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 // Create category
 categoriesRoutes.post("/", (request, response) => {
@@ -13,6 +19,11 @@ categoriesRoutes.post("/", (request, response) => {
 // List Category
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+});
+
+// Import file SVC
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
