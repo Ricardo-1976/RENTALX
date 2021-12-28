bbd7b67a1027422c1e44061538c95c87ac27c6bb
Ricardo-1976 <ricardojoaoantonio2@gmail.com>
2021-12-29 00:03:23 +0100

    Fim Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import swaggerUI from 'swagger-ui-express';
+
+import swaggerFile from "./swagger.json";
 
 import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(router);
 
