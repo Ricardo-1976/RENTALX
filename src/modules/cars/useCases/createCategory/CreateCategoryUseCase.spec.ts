b3aaafeee48c0a046b9abe8c34648d698327cc1d
Ricardo João António <90958374+Ricardo-1976@users.noreply.github.com>
2022-02-18 13:37:49 +0100

    new

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,19 +1,3 @@
-CategoriesRepositoryInMemory;
 
-import  { CreateCategoryUseCase } from "./CreateCategoryUseCase";
-
-let createCategory: CreateCategoryUseCase;
-let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
-
-describe("Create Category", () => {
-
-  beforeEach(() => {
-    categoriesRepositoryInMemory = new CategoriesRepositoryInMemoryego();
-  });
-
-  it("hould be able to create a new category", () => {
-    const createCategory = new CreateCategoryUseCase();
-  });
-});
 
 
