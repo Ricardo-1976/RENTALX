8e9be4afdbf5ba488969c841a7901cf2c5732729
Ricardo João António <90958374+Ricardo-1976@users.noreply.github.com>
2022-02-26 10:59:46 +0100

    infra

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,6 +1,6 @@
 import { response, Router } from "express";
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
-import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationContraller";
+import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
+import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationContraller";
 
 const specificationsRoutes = Router();
 
