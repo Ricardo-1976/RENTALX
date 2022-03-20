b3aee5808b23e46cbc73181033b326918661c202
Ricardo João António <90958374+Ricardo-1976@users.noreply.github.com>
2022-03-20 20:14:19 +0100

    test unitario

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,6 @@
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
+import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
   user_id: string;
@@ -6,7 +9,38 @@ interface IRequest {
 }
 
 class CreateRentalUseCase {
-  async execute(): Promise<void>;
+  constructor(private rentalsRepository: IRentalsRepository) {}
+
+  async execute({
+    user_id,
+    car_id,
+    expected_return_date,
+  }: IRequest): Promise<Rental> {
+
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavailable");
+    }
+
+    const rentalOpentToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+
+    if (rentalOpentToUser) {
+      throw new AppError("There's a rental in progress for user!");
+    }
+
+    const rental = await this.rentalsRepository.create({
+      user_id,
+      car_id,
+      expected_return_date,
+    });
+
+    return rental; 
+  }
 }
 
-export { CreateRentalUseCase }
+export { CreateRentalUseCase };
