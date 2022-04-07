13a0330543eebf91f7ec51fa92a2ea78120847a2
Ricardo-1976 <ricardojoaoantonio2@gmail.com>
2022-04-07 11:12:34 +0100

    create dayjs compare
    Compare

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -46,8 +46,6 @@ class CreateRentalUseCase {
       expected_return_date
     );
 
-      console.log("compare",compare);
-
    if (compare < minimumHour) {
      throw new AppError("Invalid return time!");
    }
